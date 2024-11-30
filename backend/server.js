02dc54071e517b6299956d13290ef33c654df3ab
manoj kumar <manojkr825313@gmail.com>
2024-11-30 19:32:21 +0530

    Add Cors for LocalHost

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const curdRoutes = require('./routes/curdRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const curdRoutes = require("./routes/curdRoutes");
 const dotenv = require("dotenv");
 
 const app = express();
 app.use(dotenv());
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(curdRoutes);
 
-mongoose.connect(process.env.MONGODB_URI)
-.then(()=>console.log("Mongo Connected"))
-.catch((err)=>console.log("mongo connected ",err));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Mongo Connected"))
+  .catch((err) => console.log("mongo connected ", err));
 
-app.listen(5000,()=>{
-     console.log("server is running");
-     
-})
+app.listen(5000, () => {
+  console.log("server is running");
+});
